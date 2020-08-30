2977cec6f3cae0b5c5ce2fccc01afa3f7140f3ab
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-08-30 14:20:13 +0900

    slight changes

--- tools/db_mgr.py
+++ tools/db_mgr.py
@@ -3,8 +3,6 @@ from tools import settings
 from tools import utils
 import psycopg2
 
-conn = None
-
 
 # region initializing and terminating db connection
 def get_db_connection():
@@ -49,32 +47,32 @@ def get_user_by_session(session_key):
     return gb_user
 
 
-def user_exists(email):
-    return get_user(email=email) is not None
-
-
 def create_or_update_user(email, name, picture, tokens):
     cur = get_db_connection().cursor(cursor_factory=psycopg2_extras.DictCursor)
 
     session_key = utils.md5(value=f'{email}{utils.now_us()}')
-    if user_exists(email=email):
-        cur.execute('update "gb_user" set name = %s, "picture" = %s, "pictureBlob" = %s, "tokens" = %s, "sessionKey" = %s where "email"=%s;', (
-            name,
-            picture,
-            utils.load_picture_bytes(picture=picture),
-            tokens,
-            session_key,
-            email
-        ))
+    if get_user(email=email) is not None:
+        cur.execute(
+            'update "gb_user" set name = %s, "picture" = %s, "pictureBlob" = %s, "tokens" = %s, "sessionKey" = %s where "email"=%s;',
+            (
+                name,
+                picture,
+                utils.load_picture_bytes(picture=picture),
+                tokens,
+                session_key,
+                email
+            ))
     else:
-        cur.execute('insert into "gb_user"("email", "name", "picture","pictureBlob","tokens","sessionKey") values (%s,%s,%s,%s,%s,%s);', (
-            email,
-            name,
-            picture,
-            utils.load_picture_bytes(picture=picture),
-            tokens,
-            session_key
-        ))
+        cur.execute(
+            'insert into "gb_user"("email", "name", "picture","pictureBlob","tokens","sessionKey") values (%s,%s,%s,%s,%s,%s);',
+            (
+                email,
+                name,
+                picture,
+                utils.load_picture_bytes(picture=picture),
+                tokens,
+                session_key
+            ))
 
     cur.close()
     get_db_connection().commit()
