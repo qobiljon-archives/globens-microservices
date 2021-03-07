1470821bb42457aa53692089c743dfa5620a66c7
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-03-08 03:26:50 +0900

    dumb equals check bugfix

--- db_scratches/2-triggers.sql
+++ db_scratches/2-triggers.sql
@@ -99,16 +99,16 @@ $gb_update_product_stars$
 declare
     reviewsCount integer;
 begin
-    if tg_op == 'INSERT' then
+    if tg_op = 'INSERT' then
         update "gb_product" set "stars" = ("stars" * "reviewsCount" + new."stars") / ("reviewsCount" + 1), "reviewsCount" = "reviewsCount" + 1 where "id" = "new"."product_id";
-    elsif tg_op == 'DELETE' then
+    elsif tg_op = 'DELETE' then
         reviewsCount := (select count(*) from "gb_product_review" where "product_id" = old."product_id");
         if reviewsCount == 1 then
             update "gb_product" set "stars" = 0.0, "reviewsCount" = 0 where "id" = "old"."product_id";
         else
             update "gb_product" set "stars" = ("stars" * "reviewsCount" - old."stars") / ("reviewsCount" - 1), "reviewsCount" = "reviewsCount" - 1 where "id" = "old"."product_id";
         end if;
-    elsif tg_op == 'UPDATE' then
+    elsif tg_op = 'UPDATE' then
         update "gb_product" set "stars" = "stars" + (new."stars" - old."stars") / "reviewsCount" where "id" = "new"."product_id";
     end if;
     return new;
