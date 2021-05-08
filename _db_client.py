3d4b85a76260c6d209f50e6401b7962b93f9986f
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-05-09 06:13:44 +0900

    category entries changed to translatable json

--- _db_client.py
+++ _db_client.py
@@ -14,30 +14,30 @@ cur = db_conn.cursor(cursor_factory=psycopg2_extras.DictCursor)
 with open('static/others.png', 'rb') as r:
     image = r.read()
     cur.execute('insert into "gb_product_category"("name", "pictureBlob", "examples") values (%s,%s,%s)', (
-        "Others",
+        json.dumps({'en': 'Others', 'ru': 'Прочие', 'uz': 'Boshqalar', 'kr': '기타'}),
         bytes(image),
-        '{"1-1 services", "Delivery"}'
+        json.dumps({'en': ["1-1 services", "Delivery"], 'ru': ['', ''], 'uz': ['', ''], 'kr': ['1-1 서비스', '배달']})
     ))
 with open('static/education.png', 'rb') as r:
     image = r.read()
     cur.execute('insert into "gb_product_category"("name", "pictureBlob", "examples") values (%s,%s,%s)', (
-        "Education",
+        json.dumps({'en': 'Education', 'ru': 'Обучение', 'uz': "Ta'lim", 'kr': '교육'}),
         bytes(image),
-        '{"Korean language", "Programming languages"}'
+        json.dumps({'en': ["Korean language", "Programming languages"], 'ru': ['Корейский язык', 'Языки программирования'], 'uz': ['Koreys tili', 'Dasturlash tillari'], 'kr': ['한국어', '개발 언어']})
     ))
 with open('static/consulting.png', 'rb') as r:
     image = r.read()
     cur.execute('insert into "gb_product_category"("name", "pictureBlob", "examples") values (%s,%s,%s)', (
-        "Consultation",
+        json.dumps({'en': 'Consultation', 'ru': 'Консультации', 'uz': 'Konsultatsiyalar', 'kr': '상담'}),
         bytes(image),
-        '{"Legal matters", "Acquiring visa"}'
+        json.dumps({'en': ["Legal matters", "Acquiring visa"], 'ru': ['Легальные вопросы', 'Получение визы'], 'uz': ['Legal savollar', 'Viza olish'], 'kr': ['법률사항', '비자']})
     ))
 with open('static/vacancies.png', 'rb') as r:
     image = r.read()
     cur.execute('insert into "gb_product_category"("name", "pictureBlob", "examples") values (%s,%s,%s)', (
-        "Vacancies",
+        json.dumps({'en': 'Vacancies', 'ru': 'Ваканции', 'uz': 'Vakansiyalar', 'kr': '결원'}),
         bytes(image),
-        '{"Assistant chef", "Software engineer", "Help desk support"}'
+        json.dumps({'en': ['Assistant chef', 'Software engineer', 'Help desk support'], 'ru': ['Помощник повара', 'Инженер программист', 'Служба поддержки'], 'uz': ['Oshpaz yordamchisi', 'Dasturiy muhandis', "Qo'llab-quvvatlash hizmati"], 'kr': ['주방장 보조', '소프트웨어 엔지니어', '헬프 데스크 지원']})
     ))
 with open('static/food.png', 'rb') as r:
     image = r.read()
